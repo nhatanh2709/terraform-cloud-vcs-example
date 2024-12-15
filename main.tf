013074fc932a9a2dd3bb2a769080a5222d9c4608
nhatanh2709 <22520059@gm.uit.edu.vn>
2024-12-15 16:11:34 +0700

    Successfully

---
+++ main.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ami" {
+    most_recent = true
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"]
+}
+
+resource "aws_instance" "server" {
+    ami = data.aws_ami.ami.id
+    instance_type = "t3.micro"
+    lifecycle {
+        create_before_destroy = true
+    }
+
+    tags = {
+        Name = "Server"
+    }
+}
